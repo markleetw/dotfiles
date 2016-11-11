67156ec481080a2ff561426050881c616d36390b
marklee <mark.lee@pinkoi.com>
2016-11-11 12:38:06 +0800

    update
    by: MarkLee <mark.lee@pinkoi.com>

---
+++ install.sh
@@ -0,0 +1,7 @@
+ln -s $(pwd)/dircolors $HOME/.dircolors
+ln -s $(pwd)/gitconfig $HOME/.gitconfig
+ln -s $(pwd)/gitmessage $HOME/.gitmessage
+ln -s $(pwd)/pinkoirc $HOME/.pinkoirc
+ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
+ln -s $(pwd)/vimrc $HOME/.vimrc
+ln -s $(pwd)/zshrc $HOME/.zshrc
