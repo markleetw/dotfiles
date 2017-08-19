1e0c3ca2915f7ec9eb049494dcf68dee523fd9fe
marklee <mark.lee@pinkoi.com>
2017-08-19 10:54:01 +0800

    rearrange installation
    by: MarkLee <me@marksylee.com>

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 ln -s $(pwd)/gitconfig $HOME/.gitconfig
 ln -s $(pwd)/gitmessage $HOME/.gitmessage
-ln -s $(pwd)/pinkoirc $HOME/.pinkoirc
-ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
-ln -s $(pwd)/vimrc $HOME/.vimrc
+
 mkdir -p $HOME/.vim/colors
+ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/vim/colors/solarized.vim $HOME/.vim/colors/solarized.vim
+
 mkdir -p $HOME/.config/nvim/colors
-ln -s ${pwd}/vim/colors/solarized.vim $HOME/.config/nvim/colors/solarized.vim
 ln -s $(pwd)/init.vim $HOME/.config/nvim/init.vim
+ln -s ${pwd}/vim/colors/solarized.vim $HOME/.config/nvim/colors/solarized.vim
+
 ln -s $(pwd)/zshrc $HOME/.zshrc
+ln -s $(pwd)/pinkoirc $HOME/.pinkoirc
+
+ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/tmuxinator $HOME/.tmuxinator
